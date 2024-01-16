3e94247397d54da8c2f31bf39ef77afe8c230f6b
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-16 17:28:49 +0100

    inserito file .json con album musicali

--- script/script.js
+++ script/script.js
@@ -10,19 +10,19 @@ createApp({
     },
 
     methods: {
-        /* getApiList() {
+        getApiList() {
             axios.get(this.apiUrl).then((response) => {
                 this.diskArray = response.data;
 
                 console.log(response.data);
             });
-        }, */
+        },
 
 
     },
 
     created() {
-        /* this.getApiList(); */
+        this.getApiList();
     }
 
 }).mount('#app')
--- server.php
+++ server.php
@@ -2,7 +2,3 @@
 $diskArray = file_get_contents('./json/diskArray.json');
 header('Content-Type: application/json');
 echo $diskArray;
-
-/* $apiData = file_get_contents('.json/diskArray.json');
-header('content-type : application/json');
-echo $apiData; */
